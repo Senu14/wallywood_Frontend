9b63eee5116409a12e3f7f82e151c89283405b72
Senu14 <101559338+Senu14@users.noreply.github.com>
2023-02-02 14:38:45 +0100

    Changes

--- src/Components/Movies/Poster.jsx
+++ src/Components/Movies/Poster.jsx
@@ -1,3 +1,5 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 import PosterStyle from './Poster.style';
 
 
@@ -6,7 +8,8 @@ import PosterStyle from './Poster.style';
   return (
     <PosterStyle>
 
-      
+      <div>Filter navigation</div>
+      <div>Plakatiste</div>
    
      <img src={poster.image} alt="" />
      <p>{poster.name}</p>
@@ -19,6 +22,35 @@ import PosterStyle from './Poster.style';
     
 
     </PosterStyle>
+  )
+}
+
+
+const GenreList = () =>{
+
+  const [ data, setData ] = useState([])
+
+  useEffect(() =>{
+    const getData = async () =>{
+      const result = await axios.get('http://localhost:4000/genre')
+      setData(result.data);
+    }
+
+    getData()
+
+  }, []);
+
+  return(
+    <ul>
+      {data && data.map(genre =>{
+        return(
+          <li key={genre.id}>
+            <Link to={`/posters/${genre.slug `}></Link>
+
+          </li>
+        )
+      })}
+    </ul>
   )
 }
 
